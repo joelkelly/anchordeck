0f10cd47fe4448934c2473f336addb8de45aa298
Joel Kelly <joel.kelly@me.com>
2013-04-23 13:29:41 -0700

    Cleanup old boilerplate

--- public/_assets/js/main.js
+++ public/_assets/js/main.js
@@ -1,10 +1,8 @@
-// Cycle Built in
-
 $(function(){
 
     $(".email").emailAddressMunging();
 
     $('.drop-click').click(function(){
         $(this).toggleClass('dropped').next().toggle();
-    })
+    });
 });
